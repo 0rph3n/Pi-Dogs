9f1fe2922c03acdb0b5a7dea839266db519fae01
0rph3n <leandrofarnochi@hotmail.com>
2023-10-21 21:41:08 -0300

    Se crean los modelos de dogs y temperaments

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,45 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imagen: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      altura: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      peso: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      tiempo_de_vida: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
