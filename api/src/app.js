49562d9336f90554060f86a19e9ccaafd613551a
0rph3n <leandrofarnochi@hotmail.com>
2023-11-08 19:28:31 -0300

    Se corrige la ruta

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-dogs-topaz-gamma.vercel.app/"
+    "https://pi-dogs-topaz-gamma.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
