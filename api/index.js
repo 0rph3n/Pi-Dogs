d04625d03cc1a0e008a9aad8ea3a14160cbfc626
0rph3n <leandrofarnochi@hotmail.com>
2023-11-10 14:58:02 -0300

    Se genero un variable de entorno para el deploy, se actualizo el llamdo de sequelize en db

--- api/index.js
+++ api/index.js
@@ -21,12 +21,12 @@ const server = require("./src/app.js");
 const getTemperaments = require("./src/controllers/getTemperaments.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const { PORT } = process.env;
+const PORT = process.env.PORT;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     getTemperaments();
-    console.log("%s listening at", PORT); // eslint-disable-line no-console
+    console.log("listening at", PORT); // eslint-disable-line no-console
   });
 });
